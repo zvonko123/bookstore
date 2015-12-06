94678558291e2736707c2baefbecca01cee7a21c
unknown <zvone@mbit.iogit config --global user.name zvone>
2015-12-06 20:39:20 +0100

    decide which table lib to use. make CRUDS

--- app/app.js
+++ app/app.js
@@ -4,9 +4,18 @@ angular.module('bookstoreApp').controller('bookstoreCtrl',
 function ($scope,$http) {
 
 
-    $scope.authors = $http
-            .get("http://localhost:49893/app/services/WebService.asmx?HelloBooks")
-            .then(function (response) { $scope.authors = response.data.records; });
+
+
+    //$scope.authors = $http
+    //        .get("http://localhost:49893/app/services/WebService.asmx?HelloBooks")
+    //        .then(function (response) { $scope.authors = response.data.records; });
+    //mock data, ask for remote db connection
+    $scope.data = {
+        "1": { "firstname": "bjarne", "lastname": "strostup" },
+        "2": { "firstname": "marko", "lastname": "zagar" },
+        "3": { "firstname": "tin", "lastname": "ujevic" },
+    }
+
 });
     
    
