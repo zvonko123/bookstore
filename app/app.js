42d6c9746060301f8818b5138bce11d82b2f656c
zvonko123 <khuuen@gmail.com>
2015-12-08 10:22:53 +0100

    -shot the ghost, wrapping ftw

--- app/app.js
+++ app/app.js
@@ -5,40 +5,54 @@ function ($scope,$http) {
 
 
 
-    $scope.bookFormShown = false;
-    $scope.booksFromAuthorShown = false;
-    //$scope.data = $http
-    //        .get("http://localhost:49893/app/services/WebService.asmx/HelloBooks")
-    //        .then(function (response) {
-    //            $scope.authors = response.data;
-    //            //$scope.authors = jQuery.xml2json($scope.authors)
-    //            $scope.authors = $scope.authors.slice(76, -9)
-    //            $scope.authors = JSON.parse($scope.authors);
-    //            console.log($scope.authors);
+   
+
+    $scope.data = $http
+          .get("http://localhost:49893/app/services/WebService.asmx/HelloBooks")
+           .then(function (response) {
+               $scope.authors = response.data;
+               //$scope.authors = jQuery.xml2json($scope.authors)
+               $scope.authors = $scope.authors.slice(76, -9)
+               $scope.authors = JSON.parse($scope.authors);
+               console.log($scope.authors);
 
                 
-    //      });
-
+          });
+    $scope.bookFormShown = false;
+    $scope.booksFromAuthorShown = false;
 
 
     //mock data, ask for remote db connection
-    $scope.authors = {
-       "0": { "FirstName": "bjarne", "LastName": "strostup" },
-       "1": { "FirstName": "marko", "LastName": "zagar" },
-       "2": { "FirstName": "tin", "LastName": "ujevic" },
-    }
+    //$scope.authors = {
+       //"0": { "FirstName": "bjarne", "LastName": "strostup" },
+       //"1": { "FirstName": "marko", "LastName": "zagar" },
+       //"2": { "FirstName": "tin", "LastName": "ujevic" },
+    //}
 
-   $scope.showNewBookForm = function ($id) {
+   $scope.showNewBookForm = function (id) {
        window.alert("Your about to add a new book !");
        $scope.bookFormShown = true;
-       $scope.formAuthor = $scope.authors[$id].FirstName + " " + $scope.authors[$id].LastName
+       $scope.findAuthor(id)
+       console.log($scope);
    }
 
    $scope.showBooksFromAuthor = function (id) {
        window.alert("displayed books from author");
        $scope.booksFromAuthorShown = true;
-       $scope.formAuthor = $scope.authors[id].FirstName + " " + $scope.authors[id].LastName;
-       $scope.books = ["prva knjiga","druga knjiga"]
+       $scope.findAuthor(id)
+       console.log($scope);
+
+        
+   }
+
+   $scope.findAuthor = function (id) {
+       for (a in $scope.authors)
+       {
+           //console.log(a);
+           if ($scope.authors[a].AuthorID == id)
+               $scope.formAuthor = $scope.authors[a];
+                }
+       
    }
   
 
